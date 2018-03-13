1cd963b151b405dd5539929fd67fc73e6d4cc034
David Daniel <davydany@gmail.com>
2018-03-12 22:08:02 -0400

    Bumped up version to 0.1.3

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/davydany/gophr',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
